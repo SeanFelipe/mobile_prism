d21a1e5d2cc575f0b91a81753e623530742400af
SeanFelipe <sean@addlightness.tech>
2020-03-18 21:27:20 -0700

    more helper methods, rreload as well

--- features/support/env.rb
+++ features/support/env.rb
@@ -14,12 +14,12 @@ desired_caps = {
     #appiumVersion:    '1.4.16',
     platformName:     'Android',
     #platformVersion:  '4.4',
-    newCommandTimeout: 600,
+    newCommandTimeout: 1200,
     automationName:       'UIAutomator2',
     deviceName:       'Nexus_5X_API_27',
     app:              ENV['APPIUM_APK'],
   }
 }
 
-$driver = Appium::Driver.new(desired_caps)
+$driver = Appium::Driver.new(desired_caps, true)
 $driver.start_driver
--- features/support/helper_methods.rb
+++ features/support/helper_methods.rb
@@ -1,8 +1,30 @@
+def rreload
+  files = [
+    './features/support/helper_methods.rb',
+    './features/step_definitions/kickstarter_steps.rb',
+  ]
+  files.each {|ff| load ff}
+end
+alias :rr :rreload
+
 def alle
   $driver.find_elements(:xpath, "//*")
 end
 
-def fa(aid)
-  $driver.find_elements(:accessibility_id, )
+def allv
+  $driver.find_elements(:class_name, "android.widget.View")
 end
 
+def alltv
+  $driver.find_elements(:class_name, "android.widget.TextView")
+end
+
+def allcn
+  els = alle
+  els.each {|el| print "#{el.className}\n"}
+  puts
+end
+
+def fa(aid)
+  $driver.find_elements(:accessibility_id, aid)
+end
