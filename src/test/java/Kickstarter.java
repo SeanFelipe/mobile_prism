b9b1dc712e19e35e2b3ea4bc870b9c7a166b20ba
Sean Felipe <sean@addlightness.tech>
2020-07-14 11:55:52 -0700

    now working with ./gradlew -i test, sees stdout

--- src/test/java/Kickstarter.java
+++ src/test/java/Kickstarter.java
@@ -2,7 +2,6 @@ import org.testng.annotations.Test;
 
 
 public class Kickstarter {
-    //@Test(groups = { "hello" })
     @Test()
     public void helloWorld() {
         System.out.println("Hello World");
